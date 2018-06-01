ee3f53ac7638090db49a599c108baa1dde2647dc
Wrothmonk <wrothmonk@gmail.com>
2018-05-31 21:05:31 -0500

    replace variable 'type' with 'c_type'
    Apparently forgot 'type' is a rather important function in lua. This fixes overlap with the var name.

--- Networking/W-Net/lib/wnet.lua
+++ Networking/W-Net/lib/wnet.lua
@@ -11,16 +11,16 @@ local devices = { --network devices
 net.drivers = drivers
 net.devices = devices
 
-function net.registerDevice(address, type, driver_path)
-  if drivers[type] == nil then
-    drivers[type] = loadfile(driver_path)()
+function net.registerDevice(address, c_type, driver_path)
+  if drivers[c_type] == nil then
+    drivers[c_type] = loadfile(driver_path)()
   end
 
   --device aspects
   local device = {
     ["address"] = address,
-    ["type"] = type,
-    ["driver"] = drivers[type]
+    ["type"] = c_type,
+    ["driver"] = drivers[c_type]
   }
 
   --create proxy to driver for device methods
@@ -39,43 +39,43 @@ function net.registerDevice(address, type, driver_path)
 
   --instert device into devices table
   devices.by_address[address] = device
-  if not devices.by_type[type] then
-    devices.by_type[type] = {n = 0}
+  if not devices.by_type[c_type] then
+    devices.by_type[c_type] = {n = 0}
   end
-  devices.by_type[type][address] = device
-  devices.by_type[type].n = devices.by_type[type].n + 1
+  devices.by_type[c_type][address] = device
+  devices.by_type[c_type].n = devices.by_type[c_type].n + 1
 
 end
 
 function net.removeDevice(address)
   --get device attributes
   local device = devices.by_address[address]
-  local type = device.type
+  local c_type = device.type
 
   --remove from device tables
-  local n = devices.by_type[type].n - 1
-  if n == 0 then --if last device of type
-    net.removeDriver(type)
+  local n = devices.by_type[c_type].n - 1
+  if n == 0 then --if last device of c_type
+    net.removeDriver(c_type)
   else
-    devices.by_type[type][address] = nil
-    devices.by_type[type].n = n
+    devices.by_type[c_type][address] = nil
+    devices.by_type[c_type].n = n
     devices.by_address[address] = nil
   end
 
 end
 
-function net.removeDriver(type)
-  drivers[type].deactivate() --call deactivation function to clean up listeners
-  drivers[type] = nil --remove driver
+function net.removeDriver(c_type)
+  drivers[c_type].deactivate() --call deactivation function to clean up listeners
+  drivers[c_type] = nil --remove driver
 
   --remove any devices relating to the driver
   --remove device count for iteration (no longer neccesary anyway)
-  devices.by_type[type].n = nil
-  for address, device in pairs(devices.by_type[type]) do
+  devices.by_type[c_type].n = nil
+  for address, device in pairs(devices.by_type[c_type]) do
     devices.by_address[address] = nil
-    devices.by_type[type][address] = nil
+    devices.by_type[c_type][address] = nil
   end
-  devices.by_type[type] = nil
+  devices.by_type[c_type] = nil
 
 end
 
